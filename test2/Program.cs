87f99ca8c30286e4d9616fa841f60254a820decd
Ashrafov Rahman <rehman199577@gmail.com>
2021-12-23 01:53:35 +0400

    firs csharp file

---
+++ test2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("giris balini daxil et: ");
+            int TelebeBali = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("imtahan balini daxil et: ");
+            int ImtahanBali = Convert.ToInt32(Console.ReadLine());
+            int Netice = TelebeBali + ImtahanBali;
+            if (Netice >= 51)
+            { 
+                Console.WriteLine("Tebrikler Siz imtahani kecdiniz"); 
+            }
+            else 
+            { 
+                Console.WriteLine("Ged birazda hazirlas"); 
+            }
+                
+
+
+
+        }
+    }
+}
