0a29663c03be484f5b8ad1a6e826261e4ed4184b
Ashrafov Rahman <rehman199577@gmail.com>
2021-12-23 23:07:52 +0400

    test

---
+++ C-sharp/ilk.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ilk
+{
+	class Program
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Bu menim ilk c sharp kodum olsun ");
+        }
+    }
+}
