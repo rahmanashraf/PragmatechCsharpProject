87932ccdaa95ea8f36f67432d51042363b2f606e
Ashrafov Rahman <rehman199577@gmail.com>
2021-12-25 01:03:16 +0400

    second csharp project ready-switch case method

---
+++ terminal/terminal/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace terminal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //balans goster
+            //pul cixar
+            //balans artir
+            //cixis ele
+            //error ver
+            int balans = 1000;
+
+            Console.WriteLine("ne etmek istediyini daxil et");
+            string emel = Console.ReadLine();
+              
+
+            switch(emel)
+            {
+                case "1"
+              
+            :   Console.WriteLine("Balansiniz " + balans+" azndir");
+                break;
+
+                case "2":
+
+                    Console.WriteLine("Neqeder cekmek isteyirdiniz: ");
+                    int cekmekistediyin=Convert.ToInt32(Console.ReadLine());
+                    if (cekmekistediyin < balans)
+                    {
+                        Console.WriteLine(cekmekistediyin + " azn mebleginde pul balansinizdan cekildi");
+                        Console.WriteLine(balans - cekmekistediyin + " " + "azn balansiniz qaldi");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Balansinizda cekmek istediyiniz qeder vesait yoxdur");
+                        break;
+                    }
+
+                    break;
+
+                case "3":
+                    Console.WriteLine("balansiniza neqeder artirmag isteyirsiniz ");
+                    int balansartirdig=Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("balansiniz artirildi ve halhazirki balansiniz " + (balans + balansartirdig) +" azndir");
+
+                    break;
+
+                case "4":
+                    Console.WriteLine("Xidmetimizden istifade etdiyiniz ucun tesekkur edirik...");
+                    break;
+
+                    
+                default: 
+                    Console.WriteLine("duzgun reqem yaz");
+                    break;
+
+            }
+            
+
+        }
+    }
+}
