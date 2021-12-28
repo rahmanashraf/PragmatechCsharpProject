22bc81338dee8613b1428c2eb2fbb308235c8372
Ashrafov Rahman <rehman199577@gmail.com>
2021-12-29 00:22:15 +0400

    for

---
+++ refoutcalc/refoutcalc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refoutcalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+        public static void vurmacedveli(ref int i)
+        {
+            for (ref int ; i <9; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
