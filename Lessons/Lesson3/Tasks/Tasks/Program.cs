3ff0ed99dee6c7969033874587ea9a291d692b0e
Ashrafov Rahman <rehman199577@gmail.com>
2022-01-01 23:38:08 +0400

    tasks test ready

---
+++ Lessons/Lesson3/Tasks/Tasks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task
+            //Asagida qeyd etdiyim algoritimleri ozunuz yazin, c#da hazir yazilmis methodlardan istifade etmeyin
+            //1)String Substring methodunun algorithmini qurun;
+            //2)Reverse methodunun algoritmi qurun : input: hello, output: olleh
+            //3)Verilmis sozun palindrome oldugunu yoxlayan algoritm mes: input: ana, output: ana(soldan ve sagdan eyni oxunur)
+            //4)Remove duplikat olan herfleri silen algoritmini yazin input: csharpcorner, output: csharpone c
+            //5)5 ədədin orta qiymətinin tapılması algoritmi qurun;
+            //6)Düzbucaqlının sahə və perimetrini tapan algoritmi qurun;
+            //7)Daxil edilən ədədin vurma cədvəlini yazdiran algoritmi qurun;
+            //8)1 le verilmis eded arasinda kvadratlari cemi algoritmini qurun;
+            //9)0 la verilmiş ədəd arasındaki ədədlərin hasili ilə cəminin fərqini hesablayan algoritmi qurun;
+            //Quize hazirlasin
+
+        }
+    }
+}
