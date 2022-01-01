57f196eb360c62cf11a77c8252283f526152d482
Ashrafov Rahman <rehman199577@gmail.com>
2022-01-02 03:11:18 +0400

    first task ready

--- Lessons/Lesson3/Tasks/Tasks/Program.cs
+++ Lessons/Lesson3/Tasks/Tasks/Program.cs
@@ -22,7 +22,32 @@ namespace Tasks
             //8)1 le verilmis eded arasinda kvadratlari cemi algoritmini qurun;
             //9)0 la verilmiş ədəd arasındaki ədədlərin hasili ilə cəminin fərqini hesablayan algoritmi qurun;
             //Quize hazirlasin
+            string soz = "kartowka";
 
+            Console.WriteLine(SUBstringo(soz,1,5));
+            Console.WriteLine(Reverse(soz));
         }
+        public static string SUBstringo(string soz,int index,int finish)
+        {
+            string yenisoz = "";
+
+            for (int i = index; i < finish ; i++)
+            {
+                yenisoz+=soz[i];
+            }
+            return yenisoz;
+            
+        }
+        public static string Reverse(string ters)
+        {
+            char[] sozyigini=ters.ToCharArray();    
+            string soz = "";
+            for (int i = sozyigini.Length - 1; i > sozyigini[0]; i--)
+            {
+                soz += ters[i];
+            }
+            return ters;
+        }
+        
     }
 }
