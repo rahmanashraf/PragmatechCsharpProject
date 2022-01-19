72fb2f37235aa13e72fbfaf4a1f73c5f0f505f11
Ashrafov Rahman <rehman199577@gmail.com>
2022-01-19 21:31:50 +0400

    added

---
+++ Scrawl-test/Medicine/Medicine/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicine
+{
+    internal class Class1
+    {
+        public string Name;
+        public int Price=0;
+        public int Count;
+        
+
+        public Class1(string medname, int medprice)
+        {
+         Name = medname;
+         Price = medprice;
+         
+            if (Price<0)
+            {
+                Console.WriteLine("0dan kiciy ola bilmez");
+            }
+            if (Count<0)
+            {
+                Console.WriteLine("0dan boyuy olmalidir");
+            }
+
+        }
+        
+
+    }
+}
---
+++ Scrawl-test/Medicine/Medicine/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1[] mehsullar = new Class1[1];
+            for (int i = 0; i < mehsullar.Length; i++)
+            {
+
+
+                Console.WriteLine("Adi daxil et: ");
+                string ad = Console.ReadLine();
+
+                Console.WriteLine("Qiymeti daxil et ");
+                int qiymet = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Sayi daxil et: ");
+                int say = Convert.ToInt32(Console.ReadLine());
+
+                Class1 std = new Class1(ad, qiymet);
+                
+
+
+                mehsullar[i] = std;
+
+            }
+            Console.WriteLine("Mehsulun adini daxil et: ");
+            string mehsuladi = Console.ReadLine();
+            Console.WriteLine("Sayi daxil et: ");
+            int sayini = Convert.ToInt32(Console.ReadLine());
+            Sell(mehsuladi, sayini,mehsullar);
+        }
+        public static void Sell(string mhsladi,int sayi, Class1[] list)
+        {
+
+            int x = 0;
+            
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i].Name == mhsladi)
+                {
+                    x = list[i].Count;
+                }
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
