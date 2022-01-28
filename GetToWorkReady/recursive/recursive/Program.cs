1e04779cb7e5db21ab7d5bbf87efc02eae3439ce
Ashrafov Rahman <rehman199577@gmail.com>
2022-01-28 15:22:49 +0400

    recursive method

---
+++ GetToWorkReady/recursive/recursive/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(F(1));
+            int toplama = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                toplama += i+1;
+                
+            }
+            Console.WriteLine(toplama);
+
+        }
+        public static int F(int x)
+        {
+            if (x==10)
+            {
+                return x;
+            }
+            else
+            {
+                return x + F(x + 1);
+            }
+            
+        }
+    }
+}
