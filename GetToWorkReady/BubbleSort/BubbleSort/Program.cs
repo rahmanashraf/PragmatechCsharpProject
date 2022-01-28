3e248b42c7524a6ba6a9326b90b02906a4c83056
Ashrafov Rahman <rehman199577@gmail.com>
2022-01-28 14:25:23 +0400

    linear algorithm

---
+++ GetToWorkReady/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 78, 55, 45, 98, 13 };
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted:");
+            foreach (int p in arr)
+                Console.Write(p + " ");
+            Console.Read();
+        }
+    }
+}
---
+++ GetToWorkReady/Sequential_Search/Sequential_Search/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sequential_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10]
+            { 1, 2, 4, 11, 20, 28, 48, 84, 96, 106};
+            int value = 20;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]==value)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine(-1);
+                }
+               
+
+            }
+    
+        }
+    }
+    
+   
+}
