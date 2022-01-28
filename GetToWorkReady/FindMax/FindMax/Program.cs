60bf7f1ec6ee917a584130d3c80d1824a801c5e3
Ashrafov Rahman <rehman199577@gmail.com>
2022-01-28 18:10:55 +0400

    FindMax added

---
+++ GetToWorkReady/FindMax/FindMax/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            int[] reqem = { 5, 1, 3, 67, 8, 34, 12, 78, 213, 23, 161,453, 23, 151 };
+            for (int i = 0; i <reqem.Length; i++)
+            {
+                for (int j = reqem.Length-1; j>-1; j--)
+                {
+                    if (reqem[i] > reqem[j])
+                    {
+                        reqem[j]=reqem[i];
+                    }
+                }
+                max = reqem[i];
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
