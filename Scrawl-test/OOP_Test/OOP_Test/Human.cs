91ccf813459df13e13ab61c3376a92c2d3bcc0ec
Ashrafov Rahman <rehman199577@gmail.com>
2022-01-31 23:58:35 +0400

    added encap

---
+++ Scrawl-test/OOP_Test/OOP_Test/Human.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Test
+{
+    internal class Human
+    {
+        private string ad;
+        protected string soyad;
+        public string fullname;
+        public string AD
+        {
+            get 
+            { 
+                return ad; 
+             
+            }
+
+            set 
+            { 
+                ad = value;
+            }
+        }
+    }
+}
---
+++ Scrawl-test/OOP_Test/OOP_Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Man yusif =new Man();
+            Human huseyn= new Human();
+            yusif.AD = "Salam";
+            huseyn.AD = "necesen";
+        }
+    }
+}
