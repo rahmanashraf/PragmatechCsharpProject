73b898d5688f381ca5b28ba8cf248a94ed9f81ef
Ashrafov Rahman <rehman199577@gmail.com>
2022-02-05 14:06:34 +0400

    ExcnahgerTask added

---
+++ Lessons/Lesson12/ValyutaTask/ValyutaTask/Exchanger.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValyutaTask
+{
+    enum CurrenctType
+    {
+        AZNtoUSD , AZNtoEURO = 2, AZNtoTL = 3,USDtoAZN= 4,EUROtoAZN=5,TLtoAZN= 6
+    }
+    static class Exchanger
+    {
+        
+        public static Dictionary<CurrenctType, double> money = new Dictionary<CurrenctType, double>();
+
+        public static void Exchange(CurrenctType valyuta, double pul)
+        {
+
+            money.Add(valyuta, pul);
+            double hesab = 0;
+            if (valyuta == CurrenctType.AZNtoUSD)
+            {
+                hesab = pul * 1.7;
+                
+            }
+            else if (valyuta == CurrenctType.AZNtoEURO)
+            {
+                hesab = pul * 2;
+                
+            }
+            else if (valyuta == CurrenctType.AZNtoTL)
+            {
+                hesab= pul * 0.13;
+                
+            }
+
+            else if (valyuta==CurrenctType.USDtoAZN)
+            {
+                hesab = pul / 1.7;
+                Console.WriteLine($"Siz daxil etdiniz " + pul + " " + " azn " + hesab + " " + " Dollar edir");
+            }
+            else if (valyuta == CurrenctType.EUROtoAZN)
+            {
+                hesab = pul / 2;
+                Console.WriteLine($"Siz daxil etdiniz " + pul + " " + " azn " + hesab + " " + " EURO edir");
+            }
+            else if (valyuta == CurrenctType.TLtoAZN)
+            {
+                hesab = pul / 0.13;
+                Console.WriteLine($"Siz daxil etdiniz " + pul + " " + " azn " + hesab + " " + " Turk Lirasi edir");
+            }
+
+            if (valyuta == CurrenctType.AZNtoUSD|| valyuta == CurrenctType.AZNtoEURO|| valyuta == CurrenctType.AZNtoTL)
+            {
+                Console.WriteLine($"Emeliyatiniz uzre {pul} miqdarinda pul {hesab} azn edir");
+
+            }
+            money.Clear();
+
+        }
+
+
+    }
+
+}
---
+++ Lessons/Lesson12/ValyutaTask/ValyutaTask/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValyutaTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            begin:
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Yerine yetirmek istediyiniz mubadile novunu seciniz:" +
+                "\n1.USD-AZN-1,70\n2.EURO-AZN-2,00\n3.TL-AZN-0,13\n4.AZN-USD-0,59\n5.AZN-EURO-0,50\n6.AZN-TL-7,98");
+
+            int enteringmoneyType = Convert.ToInt32(Console.ReadLine());
+            while (enteringmoneyType<6)
+            {
+                
+                
+                Console.WriteLine("Daxil etmek istediyiniz miqdari yazin: ");
+                double changersum = Convert.ToDouble(Console.ReadLine());
+
+                switch (enteringmoneyType)
+                {
+                    case 1:
+                   Exchanger.Exchange(CurrenctType.AZNtoUSD, changersum);
+                     break;
+                    case 2:
+                        Exchanger.Exchange(CurrenctType.AZNtoEURO,changersum);
+                        break;
+                    case 3:
+                        Exchanger.Exchange(CurrenctType.AZNtoTL, changersum);
+                        break;
+                    case 4:
+                        Exchanger.Exchange(CurrenctType.USDtoAZN, changersum);
+                        break;
+                    case 5:
+                        Exchanger.Exchange(CurrenctType.EUROtoAZN, changersum);
+                        break;
+                    case 6:
+                        Exchanger.Exchange(CurrenctType.TLtoAZN, changersum);
+                        break;
+
+                }
+                //Console.BackgroundColor = ConsoleColor.DarkRed;
+                //Console.WriteLine("Yerine yetirmek istediyiniz mubadile novunu seciniz:" +
+                //    "\n1.USD-AZN\n2.EURO-AZN\n3.TL-AZN\n4.AZN-USD\n5.AZN-EURO\n6.AZN-TL ");
+                //enteringmoneyType = Convert.ToInt32(Console.ReadLine());
+                goto begin; 
+
+            }
+        }
+    }
+}
