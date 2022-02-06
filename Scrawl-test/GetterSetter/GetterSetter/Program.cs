1764900f0971267822a4bcc43c069dc69a1b245f
Ashrafov Rahman <rehman199577@gmail.com>
2022-02-06 14:07:27 +0400

    getsettested

---
+++ Scrawl-test/GetterSetter/GetterSetter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetterSetter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std=new Student();
+            //std.Setkilo(-50);
+            std.Kilo = -50;
+            Console.WriteLine(std.Kilo);
+        }
+    }
+}
---
+++ Scrawl-test/GetterSetter/GetterSetter/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetterSetter
+{
+    internal class Student
+    {
+        private int kilo; //field
+        
+    public int Kilo { 
+            get 
+            {
+                return kilo; 
+            }
+            set
+            {
+                if (value<0)
+                {
+                    this.kilo = 0;
+                }
+                else
+                {
+                    this.kilo = value;
+                }
+            }
+        }
+    }
+}
