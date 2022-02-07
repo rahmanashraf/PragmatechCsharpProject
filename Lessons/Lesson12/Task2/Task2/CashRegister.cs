047ca0d9072d1c4a15f4fc83c78499ce9c672308
Ashrafov Rahman <rehman199577@gmail.com>
2022-02-07 04:32:36 +0400

    Currency task ready with best perform

---
+++ Lessons/Lesson12/Task2/Task2/CashRegister.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    enum Currency
+    {
+        AZN = 1, USD = 2, EURO = 3
+    }
+    enum PaymentType
+    {
+        CARD, CASH
+    }
+    internal class CashRegister
+    {
+        public int TotalAmount, TotalSalesCount = 0;
+        public Currency currency;
+
+
+        public CashRegister(int am)
+        {
+            TotalAmount = am;
+            currency = Currency.AZN;
+            
+
+        }
+        public void AddSale(string currenct, double mebleg)
+        {
+            double cem = 0;
+            if (currency == Currency.AZN && (Currency)Enum.Parse(typeof(Currency), currenct) == Currency.AZN || currency == Currency.USD && (Currency)Enum.Parse(typeof(Currency), currenct) == Currency.USD || currency == Currency.EURO && (Currency)Enum.Parse(typeof(Currency), currenct) == Currency.EURO)
+            {
+                cem = TotalAmount + mebleg;
+                Console.WriteLine("Alis verisden sonra SIZIN KASSANIZDAKI TOTAL MEBLEG:"+cem+" "+currency);
+            }
+            else
+            {
+                if (currency == Currency.AZN && (Currency)Enum.Parse(typeof(Currency), currenct) == Currency.USD)
+                {
+                    cem = TotalAmount + (mebleg * 1.70);
+                    Console.WriteLine("Alis verisden sonra SIZIN KASSANIZDAKI TOTAL MEBLEG:" + cem + " " + currency);
+                }
+                else if (currency == Currency.EURO && (Currency)Enum.Parse(typeof(Currency), currenct) == Currency.USD)
+                {
+                    cem = TotalAmount + (mebleg * 0.87);
+                    Console.WriteLine("Alis verisden sonra SIZIN KASSANIZDAKI TOTAL MEBLEG:" + cem + " " + currency);
+                }
+                else if (currency == Currency.AZN && (Currency)Enum.Parse(typeof(Currency), currenct) == Currency.EURO)
+                {
+                    cem = TotalAmount + (mebleg * 2);
+                    Console.WriteLine("Alis verisden sonra SIZIN KASSANIZDAKI TOTAL MEBLEG:" + cem + " " + currency);
+                }
+                else if (currency == Currency.EURO && (Currency)Enum.Parse(typeof(Currency), currenct) == Currency.AZN)
+                {
+                    cem = TotalAmount + (mebleg * 0.50);
+                    Console.WriteLine("Alis verisden sonra SIZIN KASSANIZDAKI TOTAL MEBLEG:" + cem + " " + currency);
+                }
+                else if (currency == Currency.USD && (Currency)Enum.Parse(typeof(Currency), currenct) == Currency.AZN)
+                {
+                    cem = TotalAmount + (mebleg * 0.58);
+                    Console.WriteLine("Alis verisden sonra SIZIN KASSANIZDAKI TOTAL MEBLEG:" + cem + " " + currency);
+                }
+                else if (currency == Currency.USD && (Currency)Enum.Parse(typeof(Currency), currenct) == Currency.EURO)
+                {
+                    cem = TotalAmount + (mebleg *1.17);
+                    Console.WriteLine("Alis verisden sonra SIZIN KASSANIZDAKI TOTAL MEBLEG:" + cem + " " + currency);
+                }
+
+            }
+
+        }
+    }
+}    
+
---
+++ Lessons/Lesson12/Task2/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+  
+
+        static void Main(string[] args)
+        {
+            begin:
+            CashRegister crg = new CashRegister(1000);
+            
+            Console.WriteLine("XOsgelmisiniz nese alirsizsa 1e basin almirsinizsa ozunuz basin");
+            string otvet=Console.ReadLine();
+            while (otvet=="1")
+            {  
+            Console.WriteLine("kassandaki pul nedi:");
+            Console.WriteLine("1.AZN" +" "+"2.USD" + " " + "3.EURO");
+            string kassa=Console.ReadLine();
+            Console.WriteLine("xerclieceyin  pul nedi:");
+            Console.WriteLine("1.AZN" + " " + "2.USD" + " " + "3.EURO");
+            string userpulu = Console.ReadLine();
+            Console.WriteLine("neqeder xerclemek isteyirsen lox: ");
+            int mebleg=Convert.ToInt32(Console.ReadLine());
+            crg.currency = (Currency)Enum.Parse(typeof(Currency), kassa);
+            Console.WriteLine("Kassanizdaki baslangic deyeri: " + crg.TotalAmount + crg.currency);
+            crg.AddSale(userpulu, mebleg);
+            goto begin;
+
+            }
+        }
+    }
+}
