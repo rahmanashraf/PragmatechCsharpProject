320ebe1565a9e07fef1c8ff123c433fbe847bc73
Ashrafov Rahman <rehman199577@gmail.com>
2022-02-09 23:16:34 +0400

    scrawltest update

---
+++ Scrawl-test/Enum_Test/Enum_Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Test
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegisterCash.carivalyuta.Add(Valyuta.AZN, 1);
+            RegisterCash.carivalyuta.Add(Valyuta.USD, 1.70);
+            RegisterCash.carivalyuta.Add(Valyuta.EURO, 2);
+            RegisterCash.carivalyuta.Add(Valyuta.TL, 0.125);
+
+            
+        }
+    }
+}
---
+++ Scrawl-test/Enum_Test/Enum_Test/RegisterCash.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Test
+{
+    enum Valyuta
+    {
+        AZN = 1,
+        USD = 2,
+        EURO = 3,
+        TL = 4
+
+    }
+    class RegisterCash
+    {
+        private static double TotalAmount;
+        private static int TotalSalesCount = 0;
+        private static Valyuta valyuta;
+
+        // ============= Data Base Sale
+        public static Dictionary<Valyuta, double> userinpulu = new Dictionary<Valyuta, double>();
+        // =========== Data Base Currency Value
+        public static Dictionary<Valyuta, double> carivalyuta = new Dictionary<Valyuta, double>();
+
+        public void SelectedOption()
+        {
+            Console.WriteLine("Cari valyutanizi secin: ");
+            Console.WriteLine(" USD : 1 ");
+            Console.WriteLine(" TL :  2 ");
+            Console.WriteLine(" ERO : 3 ");
+            Console.WriteLine(" AZN : 4 ");
+        }
+
+
+
+    }
+}
