c6140531834f019dda064a2f1d1c718464a41c7f
Ashrafov Rahman <rehman199577@gmail.com>
2022-02-11 06:11:39 +0400

    Great

---
+++ GetToWorkReady/SpecialCsharpTasks/TaskOne/Soz_oyunu/Soz_oyunu/Gaming.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soz_oyunu
+{
+    class Gaming
+    {
+        private static int Correct { get; set; }=0;
+        private static int Total { get; set; } = 0;
+        private static int Score = 0;
+        static Dictionary<string, int> ScoreMap { get; set; }
+        private static int qerar;
+        private static string name;
+        public  static string Name {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value.Length!=0)
+                {
+                
+                        name += char.ToUpper(value[0])+ value.Substring(1);          
+                    
+                }
+            }
+        }
+        public static void Go(string []array)
+        {
+            int qerar = 1;
+
+            while (qerar==1)
+            {
+
+                int index = new Random().Next(array.Length);
+                Console.WriteLine("Bundan sonra gelen sozu tapin:"+" "+array[index] + "_____");
+                
+                string answer1 = Console.ReadLine();
+                if (answer1==array[index+1])
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Duz tapdiniz!+6 bal siz qazandiniz");
+                    Score += 6;
+                    Correct++;
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Come on baby yaparsin sen.Bir herf mennen sene hediyye!:" + " " +  array[index + 1][0] + "_____");
+                    string answer2 = Console.ReadLine();
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    if (answer2==array[index+1])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Duz tapdiniz!+4bal siz qazandiniz");
+                        Score += 4;
+                        Correct++;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Come on baby yaparsin sen.Birdene de herf verirem bax bu 2 oldu bunuda tapmasan get olde day!:" + " "  +array[index+1][0]+ array[index + 1][1] + "_____");
+                        string answer3 = Console.ReadLine();
+                        Console.ForegroundColor = ConsoleColor.White;
+                        if (answer3==array[index+1])
+                        {
+                            Console.ForegroundColor=ConsoleColor.Green;   
+                            Console.WriteLine("Duz tapdiniz!+2bal siz qazandiniz");
+                            Score += 2;
+                            Correct++;
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else
+                        {
+                        begin:
+
+                            try
+                            {
+                                
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("COX malsiz kewke olsez.Duz cavab {0}\n Davam etmek ucun 1 e basin bezdizse bawka nese basin", array[index + 1]);
+                                Console.ForegroundColor = ConsoleColor.White;
+                                qerar = Convert.ToInt32(Console.ReadLine());
+                            }
+                            catch (Exception hata)
+                            {
+                                Console.WriteLine("Sehv zad eledin");
+
+                                goto begin;
+                            }
+                            
+
+                        }
+                    }
+                }
+                
+                Total++;
+                
+            }
+
+
+
+
+        }
+        public static void Count(object obj)
+        {
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Cox hormetli {name}! neticeleriniz asagidaki kimidir bro");
+
+            Console.WriteLine("Toplam balin: " + Gaming.Score);
+            Console.WriteLine("Toplam verdiyin cavab sayi " + Gaming.Total);
+            Console.WriteLine("Verdiyin duzgun cavabsayi: " + Gaming.Correct);
+            int wrong = Gaming.Total - Gaming.Correct;
+            Console.WriteLine("Elediyin mallig sayi: " + wrong);
+            qerar = 2;
+            
+        }
+        
+    }
+}
--- GetToWorkReady/SpecialCsharpTasks/TaskOne/Soz_oyunu/Soz_oyunu/Program.cs
+++ GetToWorkReady/SpecialCsharpTasks/TaskOne/Soz_oyunu/Soz_oyunu/Program.cs
@@ -2,106 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-using System.Timers;
+
 
 
 namespace Soz_oyunu
 {
     internal class Program
     {
-        private static int x = 120;
-
         static void Main(string[] args)
         {
-            string authors = "You can gain experience, if you are careful to avoid empty redundancy. Do not fall into the error of the artisan who boasts of twenty years experience in craft while in fact he has had only one year of experience twenty times. And never resent the advantage of experience your elders have. Recall that they have paid for this experience in the coin of life, and have emptied a purse that cannot be refilled.";
+            string authors = "You can gain experience, if you are careful to avoid empty redundancy. Do not fall into the error of the artisan who boasts of twenty years experience in craft while in fact he has had only one year of experience twenty times. And never resent the advantage of experience your elders have. Recall that they have paid for this experience in the coin of life, and have emptied purse that cannot be refilled.";
+
+            string[] spearator = { " ", ".", ", ", "-", };
+
+            int count = 80;
+            string[] words = authors.Split(spearator, count,
+               StringSplitOptions.RemoveEmptyEntries);
+
+            begin:
+
+            Console.WriteLine("Yeni oyuna baslamag ucun 1 duymesini bas:");
+            string cavab = Console.ReadLine();
+            Console.WriteLine("Adinizi qeyd edin bro: ");
+            Gaming.Name = Console.ReadLine();
+            if (cavab == "1")
+            {
+                int num = 0;
+                TimerCallback tm = new TimerCallback(Gaming.Count);
+                Timer timer = new Timer(tm, num, 10000, 0);
+                Gaming.Go(words);
+
+            }
+            else
+            {
+                goto begin;
+            }
 
 
-            string[] words = authors.Split(' ', ',', '.', '"');
 
-            //Random random = new Random();
-            
-            Timer time = new Timer(5000); //to milliseconds
-            time.Enabled = true;
-            time.Elapsed += new ElapsedEventHandler(MyHandler);
-
-            time.Start();
-            Console.WriteLine("soz yaz");
-            string sozyaz = Console.ReadLine();
-            Console.WriteLine(sozyaz);
-            time.Start();
-
-           
-        
-
-
-
-
-        //Timering(words,random);
-
-
-
-        
-            //timer.Interval = 1000;
-            //timer.Elapsed += Timer_Elapsed;
-            //RandomFind(words);
-            //timer.Start();
-
-}
-        private static void MyHandler(object e, ElapsedEventArgs args)
-        {
-            var timer = (Timer)e;
-            Console.WriteLine("vaxt bitdi");
-            timer.Stop();
         }
 
-
-
-        //private static void Timering(string []words,Random random)
-        //{
-
-        //    for (int i = 10; i >= 0; i--)
-        //    {
-
-
-        //        Thread.Sleep(1000);
-        //        Console.WriteLine(i);
-        //if (i == 0)
-        //{
-
-        //Console.WriteLine(i);
-        //int osoz = random.Next(words.Length);
-        //int result = 0;
-        //string duz_soz = words[osoz] + " " + words[osoz + 1];
-        //string ilksoz = words[osoz];
-        //string sonsoz = words[osoz + 1];
-        ////Console.WriteLine(duz_soz);
-        //Console.WriteLine(duz_soz + " bax ama elebele yoxlamagcundu");
-        //Console.WriteLine(ilksoz + "______");
-        ////Console.WriteLine(sonsoz);
-
-        //Console.WriteLine("ikinci sozu tap:");
-        //string cavab1 = Console.ReadLine();
-        //if (cavab1 == sonsoz)
-        //{
-        //    result += 6;
-        //}
-        //Console.WriteLine("yekun baliniz" + result);
-
-
-        //}
-        //else
-        //{
-        //    Console.WriteLine(i + " " + "saniyeniz qaldi ve muddetiniz basha catdi normalda bu hisse de sizin neticeleriniz olacag");
-        //}
-
-
-
-        //public static void RandomFind(string [] array)
-        //{
-
-
-
-        //}
     }
 }
