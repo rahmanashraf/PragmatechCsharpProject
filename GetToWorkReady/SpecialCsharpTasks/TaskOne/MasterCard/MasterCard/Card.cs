2e120bf012c0935a960fdb3788972ec0ef0df4a7
Ashrafov Rahman <rehman199577@gmail.com>
2022-02-11 22:55:41 +0400

    Loading Task

---
+++ GetToWorkReady/SpecialCsharpTasks/TaskOne/MasterCard/MasterCard/Card.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterCard
+{
+    internal class Card
+    {
+        private long cardno;
+        public long CardNo
+        {
+            get
+            {
+                return cardno;
+            }
+            set
+            {
+                Random rnd = new Random();
+                string cardNumber = string.Empty;
+
+                for (int i = 0; i < 16; i++)
+                {
+                    cardNumber += rnd.Next((int)value, 9).ToString();
+                }
+                value = long.Parse(cardNumber);
+                cardno = value;
+            }
+
+        }
+        private  int money;
+        public  int Money
+        {
+            get
+            {
+                return money;
+            }
+            set
+            {
+                money =value;
+            }
+
+        }
+
+        public DateTime data = DateTime.Now;
+        public List<int> list=new List<int>();    
+
+        public Card(long reqem,int mebleg,int days)
+        {
+            cardno = reqem;
+            money = mebleg;
+            data=DateTime.Now.AddDays(days);
+            Console.WriteLine("Bu menim konstruktordan gelen ilk kartimdi"+ "\n16 reqemli sifre "+cardno+" "+"\nBalans"+money+"azn "+"\nBitme tarixi"+" "+data);
+        }
+
+        public void AddVirtualCard()
+        {
+            Random rndm = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                CardNo = i;
+                Money = rndm.Next(i, 200);
+                Console.WriteLine(CardNo+" "+ Money);
+            }
+        }
+
+
+    }
+}
--- GetToWorkReady/SpecialCsharpTasks/TaskOne/MasterCard/MasterCard/Program.cs
+++ GetToWorkReady/SpecialCsharpTasks/TaskOne/MasterCard/MasterCard/Program.cs
@@ -11,6 +11,19 @@ namespace MasterCard
         static void Main(string[] args)
         {
 
+
+            Card card1=new Card(43323425253322529,1000,50);
+                 
+            card1.AddVirtualCard();
+            card1.AddVirtualCard();
+            card1.AddVirtualCard();
+            card1.AddVirtualCard();
+            card1.AddVirtualCard();
+
+
+            Console.WriteLine(card1.Money);
+
+
         }
     }
 }
