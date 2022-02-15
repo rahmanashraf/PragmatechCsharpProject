b116757906548f7f4d476c4f8e8179b6ea0cb53f
Ashrafov Rahman <rehman199577@gmail.com>
2022-02-15 23:55:13 +0400

    Task is ready

---
+++ GetToWorkReady/SpecialCsharpTasks/TaskOne/Bank/Bank/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MainCard mcard = new MainCard();
+            
+            Console.WriteLine("Esas kartinda neqeder mebleg olsun");
+            double esasmebleg = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Neche gun vaxti olsun kartinin");
+            int gun = Convert.ToInt32(Console.ReadLine());
+            mcard.MaindCardAdd(esasmebleg, gun);
+            Console.WriteLine("Hansi emeliyati elemey istierdiniz:\n1.Yeni virtualkartin elave olunmasi.\n2.VirtualKart silmek.\n3.Esas kartinin balansini oyren.\n4.Butun kartlari goster.\n5.Proqramdan cix");
+            int cavab = Convert.ToInt32(Console.ReadLine());
+            while (cavab < 5)
+            {
+
+
+                if (cavab == 1)
+                {
+                    Console.WriteLine("VirtualCardinin neqeedr icinde pul olsun");
+                    double vrtmebleg = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("VirtualCardinin nece gun muddeti olsn");
+                    int gunvirtual = Convert.ToInt32(Console.ReadLine());
+                    VirtualCard vcard = new VirtualCard(vrtmebleg,gunvirtual);
+                    VirtualCard.AddList(vcard);
+                }
+                else if (cavab == 2)
+                {
+                    
+                    VirtualCard.RemoveList();
+                }
+                else if (cavab == 3)
+                {
+                    Console.WriteLine($"Sizin esas kartinizda movcud olan halhazirki balans { MainCard.Balansim}azn dir");
+                }
+
+                else if (cavab == 4)
+                {
+                    VirtualCard.ShowList();
+                }
+               
+
+                Console.WriteLine("Hansi emeliyati elemey istierdiniz:\n1.Yeni virtualkartin elave olunmasi.\n2.VirtualKart silmek.\n3.Esas kartinin balansini oyren.\n4.Butun kartlari goster.\n5.Proqramdan cix");
+                cavab = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Tesekkur edirik birat");
+
+
+
+
+
+        }
+    }
+}
---
+++ GetToWorkReady/SpecialCsharpTasks/TaskOne/Bank/Bank/VirtualCard.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class VirtualCard
+    {
+        static List <VirtualCard> cardlist = new List<VirtualCard>();
+        string Cardno;
+        DateTime Date;
+        double Balans;
+        public VirtualCard(double mebleg, int gun)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 16; i++)
+            {
+                Cardno += random.Next(0, 10).ToString();
+            }
+            Console.WriteLine("kart hazirdi nomresi" + " " + Cardno);
+
+            if (mebleg < MainCard.Balansim)
+            {
+                if (MainCard.Datem > Date)
+                {
+                    MainCard.Balansim = MainCard.Balansim - mebleg;
+                    Balans = mebleg;
+                    Date = DateTime.Now.AddDays(gun);
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Virtualin tarixi esasdan uzun ola bilmez");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Virtualin Balansi Main balansdan cox ol abilmez(");
+            }
+        }
+        //public void AddVirtualCard()
+        //{
+        //    Cardno = "";
+        //    Random random = new Random();
+        //    for (int i = 0; i < 16; i++)
+        //    {
+        //        Cardno += random.Next(0, 10).ToString();
+        //    }
+        //    Console.WriteLine("kart hazirdi nomresi"+" "+Cardno);
+
+        //    if (mebleg<MainCard.Balansim)
+        //    {
+        //        if (MainCard.Datem>Date)
+        //        {
+        //            MainCard.Balansim = MainCard.Balansim - mebleg;
+        //            Balans = mebleg;
+        //            Date = DateTime.Now.AddDays(gun);
+                    
+
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("Virtualin tarixi esasdan uzun ola bilmez");
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Virtualin Balansi Main balansdan cox ol abilmez(");
+        //    }
+
+        //}
+
+        public static void AddList(VirtualCard card)
+        {
+            if (cardlist.Count <= 5)
+            {
+                cardlist.Add(card);
+                
+            }
+            else
+            {
+                Console.WriteLine("Kart limitiniz dolmusdur bundan artiq kart yarada bilmersiniz");
+            }
+        }
+
+        public static void RemoveList()
+        {
+            if (cardlist.Count==0)
+            {
+                Console.WriteLine("Silinesi kartiniz yoxdur");
+            }
+            else
+            {
+                Console.WriteLine("Halhazirda bu kartlariniz movcuddur: ");
+            for (int i = 0; i < cardlist.Count; i++)
+            {
+                int sirasayi = i + 1;
+                Console.WriteLine($"{sirasayi}nomreli kartiniz \n KartNomresi: {cardlist[i].Cardno} \n Bitme tarixi: {cardlist[i].Date} \n Balansi: {cardlist[i].Balans}");
+
+            }
+            Console.WriteLine("Nechenci karti silmek istediyini yaz");
+            int indexim = Convert.ToInt32(Console.ReadLine());
+            MainCard.Balansim = MainCard.Balansim + cardlist[indexim-1].Balans;
+            cardlist.RemoveAt(indexim);
+            }
+            
+        }
+        public static void ShowList()
+        {
+            if (cardlist.Count == 0)
+            {
+                Console.WriteLine("Siz hele kart elave etmemisiniz");
+
+            }
+            else
+            {
+                for (int i = 0; i < cardlist.Count; i++)
+                {
+                    int sirasayi = i + 1;
+                    Console.WriteLine($"{sirasayi}nomreli kartiniz \n KartNomresi: {cardlist[i].Cardno} \n Bitme tarixi: {cardlist[i].Date} \n Balansi: {cardlist[i].Balans}");
+
+                }
+            }
+        }
+
+        
+    }
+}
