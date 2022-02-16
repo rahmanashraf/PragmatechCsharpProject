c04b740362122745246dc4b33b781339a9949485
Ashrafov Rahman <rehman199577@gmail.com>
2022-02-16 19:16:52 +0400

    Update Folder Structure

---
+++ GetToWorkReady/Type_of_Algo/FindMin/FindSameNumber/FindSameNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindSameNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int say =0;
+            int e=0;    
+            int[] array1 = { 42, 23, 15, 7, 16, 2 };
+            int[] array2 = { 41, 23, 12, 7, 12, 25 };
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    if (array1[i]==array2[j])
+                    {
+                        e=array2[i];
+                        Console.WriteLine(e + "reqemi her iki arrayda da movcuddur");
+
+                        say++;
+                    }
+                }
+
+            }
+            Console.WriteLine("iki arrayin"+say+" "+ "sayida ortag reqemi var");
+
+        }
+    }
+}
---
+++ GetToWorkReady/Type_of_Algo/FindMin/Sorting_Algorithms/Sorting_Algorithms/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] array = { 6, 1, 26, 4, 9, 2, 89, 3, 5 };
+            int[] array2 = new int[array.Length];
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                int place = 0;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        place = place + 1;
+                    }
+                    
+                }
+                array2[place] = array[i];
+            }
+            foreach (int item in array2)
+            {
+                Console.WriteLine(item);
+            }   
+            
+        }
+    }
+}
