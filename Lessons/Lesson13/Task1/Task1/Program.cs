316d5aa366a13850d36334b432b30231c3687b9e
Ashrafov Rahman <rehman199577@gmail.com>
2022-02-18 02:34:01 +0400

    Task1_Ready

---
+++ Lessons/Lesson13/Task1/Task1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    interface IFigure
+    {
+        void CalculateArea();
+        void CalculatePerimeter();
+
+    }
+    class Rectangle : IFigure
+    {
+        public void CalculateArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculatePerimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Circle : IFigure
+    {
+        public void CalculateArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculatePerimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Triangle : IFigure
+    {
+        public void CalculateArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculatePerimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Square : IFigure
+    {
+        public void CalculateArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculatePerimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
