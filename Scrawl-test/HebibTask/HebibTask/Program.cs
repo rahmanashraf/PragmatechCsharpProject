4ee4049ba6b30fdff2159ad1f9b9c7858f3c9e63
Ashrafov Rahman <rehman199577@gmail.com>
2022-02-27 23:41:20 +0400

    scrawltest

---
+++ Scrawl-test/HebibTask/HebibTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HebibTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] array = {10,20,50,100};
+            int boyuk=0;
+            foreach (var arr in array)
+            {
+                foreach (var item in array)
+                {
+                    if (arr > item)
+                    {
+                        Console.WriteLine(arr);
+                    }
+                    else
+                    {
+                        boyuk = item;
+                    }
+                }
+                Console.WriteLine("En boyuk reqem budur" + " " + boyuk);
+
+            }
+            
+            //Console.WriteLine("Cekmek istediyiniz pulu daxil edin");
+            //int gelenpul = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
