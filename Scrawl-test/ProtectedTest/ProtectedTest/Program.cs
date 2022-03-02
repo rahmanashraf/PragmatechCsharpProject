abb489a6f17033a4124915682e7447bf299757a4
Ashrafov Rahman <rehman199577@gmail.com>
2022-03-02 17:28:34 +0400

    acces modified problem fixed

---
+++ Scrawl-test/ProtectedTest/ProtectedTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtectedTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Protected mal=new Protected();
+            mal.Sagolde();
+            mal.Salamver();
+            mal.Hecnedeme();
+        }
+    }
+
+   
+}
---
+++ Scrawl-test/ProtectedTest/ProtectedTest/Protected.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtectedTest
+{
+    internal class Protected
+    {
+
+        protected void Salamver()
+        {
+
+        }
+        public void Sagolde()
+        {
+
+        }
+        internal void Hecnedeme()
+        {
+
+        }
+    }
+}
