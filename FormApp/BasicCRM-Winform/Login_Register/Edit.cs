6099481e61c6f74eeb04474d8ccb86fa79d878b8
Ashrafov Rahman <rehman199577@gmail.com>
2022-03-07 04:04:08 +0400

    passwordchecker

--- FormApp/BasicCRM-Winform/Login_Register/Edit.cs
+++ FormApp/BasicCRM-Winform/Login_Register/Edit.cs
@@ -28,7 +28,33 @@ namespace Login_Register
             login.Show();
 
         }
-
+        private bool PasswordCheck()
+        {
+            bool result = false;
+            string boyukherf = Edit_Pass.Text;
+            int netice = 0;
+            int esasnetice = 0;
+            boyukherf.ToUpper();    
+            if (Edit_Pass.Text.Length>8&&Edit_Pass.Text.Length<16)
+            {
+                for (int i = 0; i < Edit_Pass.Text.Length; i++)
+                {
+                    if (Edit_Pass.Text[i]==boyukherf[i])
+                    {
+                        netice++;
+                    }
+                    if (netice>0 && char.IsDigit(Edit_Pass.Text[i]))
+                    {
+                        esasnetice++;
+                    }
+                }
+            }
+            if (esasnetice>0)
+            {
+                result = true;
+            }
+            return result;
+        }
         private void Edit_Save_Click(object sender, EventArgs e)
         {
             foreach (Users item in Lists.Userlist)
@@ -36,7 +62,15 @@ namespace Login_Register
                 if (EnteringUser.EditingName==item.Email)
                 {
                     item.Email = Edit_Mail.Text;
-                    item.Password = Edit_Pass.Text;
+                    if (PasswordCheck())
+                    {
+                        item.Password = Edit_Pass.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Parolu duzgun daxil edin");
+                    }
+                    
                 }
                               
             }
