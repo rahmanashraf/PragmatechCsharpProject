fd0d3a383e8362941b67dc1a1c144f1be3de2063
Ashrafov Rahman <rehman199577@gmail.com>
2022-03-27 18:53:37 +0400

    comeback

---
+++ Scrawl-test/PhotoGallery/PhotoGallery/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoGallery
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            pictureBox1.Width = 200;
+            pictureBox1.Height=200; 
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.ImageLocation = openFileDialog1.FileName;
+            //foreach (var item in openFileDialog1.FileNames)
+            //{
+                MessageBox.Show(openFileDialog1.FileName);
+            //}
+
+
+        }
+    }
+}
