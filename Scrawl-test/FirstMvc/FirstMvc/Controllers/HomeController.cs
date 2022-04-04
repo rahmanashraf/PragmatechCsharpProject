9e05a5fe9869eaae7177676d24e71b109aaeb267
Ashrafov Rahman <rehman199577@gmail.com>
2022-04-04 22:57:24 +0400

    update

---
+++ Scrawl-test/FirstMvc/FirstMvc/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using FirstMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FirstMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Index()
+        {
+            return "index";
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
