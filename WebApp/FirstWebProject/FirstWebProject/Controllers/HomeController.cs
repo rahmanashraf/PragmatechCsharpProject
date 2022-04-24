835c2f99e1f54f7817060dff7055bbb361eaeec2
Ashrafov Rahman <rehman199577@gmail.com>
2022-04-24 17:07:34 +0400

    newproject created

---
+++ WebApp/FirstWebProject/FirstWebProject/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using FirstWebProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstWebProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GET: Home
+        WebAppTestEntities wbtest = new WebAppTestEntities() ;
+       
+        public ActionResult Index()
+        {
+            var b=Request.QueryString["name"];
+            
+            var wbtestim=new WebTest() { content = b };
+            wbtest.WebTest.Add(wbtestim);
+            ViewBag.list = wbtest.WebTest.ToList();
+            wbtest.SaveChanges();
+            return View();
+        }
+      
+        
+
+
+    }
+}
