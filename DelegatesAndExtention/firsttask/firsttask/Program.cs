43a5407176151daf4d36a63bad73e0fcd8cd1a45
Ashrafov Rahman <rehman199577@gmail.com>
2022-05-03 23:57:51 +0400

    delete folder

---
+++ DelegatesAndExtention/firsttask/firsttask/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firsttask
+{
+    delegate void StringFilter(string str);
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<string> StringFilter(string ls, StringFilterDelegate del)
+            {
+                // Funksiya aşağıdakı əməlyatları yerinə yetirə bilməlidir
+                // List daxilindəki sözlərdən hərf sayı 5-dən böyük olanlar
+                // Daxilində ən az bir ədəd böyük hərf olanlar
+                // Baş hərfi böyük olanlar
+                // Daxilində ə hərfi olanlar
+            }
+        }
+    }
+}
