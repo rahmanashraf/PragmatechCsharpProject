dcfe0724deb3571a147f9d2654cec2556d84a77e
Ashrafov Rahman <rehman199577@gmail.com>
2022-05-05 22:41:04 +0400

    update

--- DelegatesAndExtention/DelegateTask/firsttask/Program.cs
+++ DelegatesAndExtention/DelegateTask/firsttask/Program.cs
@@ -6,20 +6,25 @@ using System.Threading.Tasks;
 
 namespace firsttask
 {
-    delegate void StringFilter(string str);
+    delegate void StringFilterDelegate(string str);
     internal class Program
     {
         
         static void Main(string[] args)
         {
-            List<string> StringFilter(string ls, StringFilterDelegate del)
-            {
+
+            List<string> list = new List<string>() { "salam", "sagol", "netersen", "xiyaristan", "abdulla" };
+            StringFilter("soz", StringFilterDelegate del);
                 // Funksiya aşağıdakı əməlyatları yerinə yetirə bilməlidir
                 // List daxilindəki sözlərdən hərf sayı 5-dən böyük olanlar
                 // Daxilində ən az bir ədəd böyük hərf olanlar
                 // Baş hərfi böyük olanlar
                 // Daxilində ə hərfi olanlar
-            }
+            
+        }
+        public List<string> StringFilter(string ls, StringFilterDelegate del)
+        {
+            return del;
         }
     }
 }
---
+++ DelegatesAndExtention/Hackerrank/StrongPasswordHACKERRANK/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrongPasswordHACKERRANK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///parol lazimdi 6 herfli olmaliid en azii
+            ///bir xarakter bir lower bir upper olmalidi
+            ///input parol olanda neche karakter eksiydise  dimelidi ama sirf uzunliga nisbeten nece xarakter catmir o yox
+            ///umumietle nece dene sey yazsan duzelecey onu istiyir
+            ///
+
+            string special_characters = "!@#$%^&*()-+"; 
+            Console.WriteLine("Daxil et parol");
+            string password=Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(password)&&password.Length>5)
+            {
+                Console.WriteLine("daxil olduz");
+
+            }
+            else
+            {
+
+            }
+
+
+        }
+    }
+}
