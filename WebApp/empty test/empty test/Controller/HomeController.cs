3168772e55488a8c5710b039cef5c076605f6b55
Ashrafov Rahman <rehman199577@gmail.com>
2022-05-09 23:36:40 +0400

    aspnet core empty web project testing create mvc

---
+++ WebApp/empty test/empty test/Controller/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace empty_test.Controller
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
