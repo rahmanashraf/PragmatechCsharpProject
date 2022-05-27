5f9ba12cd867d02699f6e66c1095a2263c3fe1b2
Ashrafov Rahman <rehman199577@gmail.com>
2022-05-27 14:33:20 +0400

    green garden project finish

---
+++ Scrawl-test/TwoSum/TwoSum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] nums = {3,2,4};
+            
+            int target = 6;
+            int [] newarray= TwoSum(nums,target);
+            for (int i = 0; i < newarray.Length; i++)
+            {
+            Console.WriteLine(newarray[i]);
+            }
+        }
+        public static int[] TwoSum(int[] nums, int target)
+        {
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i - j != 0 && target - nums[i] == nums[j])
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+    }
+
---
+++ WebApp/Aspnetcoreempty/Aspnetcoreempty/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aspnetcoreempty.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
