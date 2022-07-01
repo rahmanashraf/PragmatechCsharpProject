08312e008f3d26499dc48d7adf42be91cc6b0136
Ashrafov Rahman <rehman199577@gmail.com>
2022-07-01 17:18:54 +0400

    update all changing

---
+++ FormApp/AghdashSales/AghdashSales/Models/Testesteron.cs
@@ -0,0 +1,22 @@
+﻿namespace AghdashSales.Models
+{
+    public class Testesteron
+    {
+
+        public class Malsiyahi
+        {
+            public int Id { get; set; } 
+            public string Name { get; set; }
+            public float Maya { get; set; } 
+            public int Say { get; set; }
+            public int Satis { get; set; }
+
+        }
+
+        public class Satis
+        {
+            public int Id { get; set; }
+            public int Malsiyahi   
+        }
+    }
+}
